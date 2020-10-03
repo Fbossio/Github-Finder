99ff451fd686bd5246aaf59f818807aff18efa01
Felix Bossio <felixbossio@gmail.com>
2020-10-03 01:56:18 -0500

    Add axios to the project

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -20,6 +21,10 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       </a>
     </div>
   );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 };
 
 export default UserItem;
