5aea4685f156a1a319e0890c7f82f85e1a7a980a
Felix Bossio <felixbossio@gmail.com>
2020-10-03 01:32:40 -0500

    Refactor UserItem as a functional component

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <a
-          href={html_url}
-          className="btn btn-dark btn-sm my-1"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          More
-        </a>
-      </div>
-    );
-  }
-}
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <a
+        href={html_url}
+        className="btn btn-dark btn-sm my-1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        More
+      </a>
+    </div>
+  );
+};
 
 export default UserItem;
